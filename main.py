6681a093e5c799bd6342bec05ac6b0fda8ec585d
Steven Rice <TheStevenRice@Gmail.com>
2022-04-23 16:59:06 -0400

    Re-added Linear Regression and SGD Regression

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 
+# Regression Models
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import SGDRegressor
+
 # Neural Network Model
 from sklearn.neural_network import MLPClassifier
 
@@ -11,6 +15,9 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 import matplotlib.pyplot as plt
+
+# Set to true to display plots.
+do_plots = True
 
 # Loading data from the CSV file.
 data = pd.read_csv('mushrooms.csv', header=None)
@@ -64,7 +71,7 @@ for x in range(1, 10):
 #       If “log2”, then max_features=log2(n_features).
 #       If None, then max_features=n_features.
 
-DecisionTreeClassifier_model = DecisionTreeClassifier(
+model_DecisionTreeClassifier = DecisionTreeClassifier(
     criterion='gini',
     splitter='best',
     max_depth=None,
@@ -97,7 +104,7 @@ DecisionTreeClassifier_model = DecisionTreeClassifier(
 #       Leaf size passed to BallTree or KDTree. This can affect the speed of the construction and query, as well as the
 #       memory required to store the tree. The optimal value depends on the nature of the problem.
 
-KNN_model = KNeighborsClassifier(
+model_KNeighborsClassifier = KNeighborsClassifier(
     n_neighbors=5,
     weights='uniform',
     algorithm='auto',
@@ -151,7 +158,7 @@ KNN_model = KNeighborsClassifier(
 #       Whether to shuffle samples in each iteration.
 #       Only used when solver=’sgd’ or ‘adam’.
 #
-MLPClassifier_model = MLPClassifier(
+model_MLPClassifier = MLPClassifier(
     hidden_layer_sizes=(100, 100),
     activation='relu',
     solver='adam',
@@ -163,8 +170,107 @@ MLPClassifier_model = MLPClassifier(
 )
 
 
-# Set to true to display plots.
-do_plots = False
+# Linear Regressions Model
+#
+#   fit_intercept - bool, default=True
+#       Whether to calculate the intercept for this model. If set to False, no intercept will be used in calculations
+#       (i.e. data is expected to be centered).
+#
+#   copy_X - bool, default=True
+#       If True, X will be copied; else, it may be overwritten.
+#
+#   n_jobs - int, default=None
+#       The number of jobs to use for the computation. This will only provide speedup in case of sufficiently large
+#       problems, that is if firstly n_targets > 1 and secondly X is sparse or if positive is set to True. None means 1
+#       unless in a joblib.parallel_backend context. -1 means using all processors.
+#
+model_LinearRegression = LinearRegression(
+    fit_intercept=True,
+    copy_X=True,
+    n_jobs=None
+)
+
+
+# Stochastic Gradient Descent Regressor
+#
+#   loss - str, default=’squared_error’
+#       The loss function to be used. The possible values are:
+#       ‘squared_error’, ‘huber’, ‘epsilon_insensitive’, or ‘squared_epsilon_insensitive’
+#
+#       The ‘squared_error’ refers to the ordinary least squares fit. ‘huber’ modifies ‘squared_error’ to focus less on
+#       getting outliers correct by switching from squared to linear loss past a distance of epsilon.
+#       ‘epsilon_insensitive’ ignores errors less than epsilon and is linear past that; this is the loss function used
+#       in SVR. ‘squared_epsilon_insensitive’ is the same but becomes squared loss past a tolerance of epsilon.
+#
+#   penalty = {‘l2’, ‘l1’, ‘elasticnet’}, default=’l2’
+#       The penalty (aka regularization term) to be used. Defaults to ‘l2’ which is the standard regularizer for linear
+#       SVM models. ‘l1’ and ‘elasticnet’ might bring sparsity to the model (feature selection) not achievable with ‘l2’
+#
+#   alpha - float, default=0.0001
+#       Constant that multiplies the regularization term. The higher the value, the stronger the regularization. Also
+#       used to compute the learning rate when set to learning_rate is set to ‘optimal’.
+#
+#   l1_ratio - float, default=0.15
+#       The Elastic Net mixing parameter, with 0 <= l1_ratio <= 1. l1_ratio=0 corresponds to L2 penalty, l1_ratio=1 to L1.
+#       Only used if penalty is ‘elasticnet’.
+#
+#   fit_intercept - bool, default=True
+#       Whether the intercept should be estimated or not. If False, the data is assumed to be already centered.
+#
+#   max_iter - int, default=1000
+#       The maximum number of passes over the training data (aka epochs). It only impacts the behavior in the fit method,
+#       and not the partial_fit method.
+#
+#   tol - float, default=1e-3
+#       The stopping criterion. If it is not None, training will stop when (loss > best_loss - tol) for n_iter_no_change
+#       consecutive epochs. Convergence is checked against the training loss or the validation loss depending on the
+#       early_stopping parameter.
+#
+#   shuffle - bool, default=True
+#       Whether or not the training data should be shuffled after each epoch.
+#
+#   verbose - int, default=0
+#       The verbosity level.
+#
+#   epsilon - float, default=0.1
+#       Epsilon in the epsilon-insensitive loss functions; only if loss is ‘huber’, ‘epsilon_insensitive’, or
+#       ‘squared_epsilon_insensitive’. For ‘huber’, determines the threshold at which it becomes less important to get the
+#       prediction exactly right. For epsilon-insensitive, any differences between the current prediction and the correct
+#       label are ignored if they are less than this threshold.
+#
+#
+#   learning_rate - str, default=’invscaling’
+#       The learning rate schedule:
+#
+#       ‘constant’: eta = eta0
+#       ‘optimal’: eta = 1.0 / (alpha * (t + t0)) where t0 is chosen by a heuristic proposed by Leon Bottou.
+#       ‘invscaling’: eta = eta0 / pow(t, power_t)
+#
+#       ‘adaptive’: eta = eta0, as long as the training keeps decreasing. Each time n_iter_no_change consecutive epochs
+#       fail to decrease the training loss by tol or fail to increase validation score by tol if early_stopping is
+#       True, the current learning rate is divided by 5.
+#
+#   eta0 - float, default=0.01
+#       The initial learning rate for the ‘constant’, ‘invscaling’ or ‘adaptive’ schedules. The default value is 0.01.
+#
+#   power_t - float, default=0.25
+#       The exponent for inverse scaling learning rate.
+#
+
+model_SGDRegressor = SGDRegressor(
+    loss='squared_error',
+    penalty='l2',
+    fit_intercept=True,
+    max_iter=1000,
+    tol=0.001,
+    shuffle=True,
+    verbose=0,
+    epsilon=0.1,
+    learning_rate='invscaling',
+    eta0=0.01,
+    power_t=0.25
+)
+
 
 # Looping over training/testing sets, train model, calculate prediction, print accuracy.
 for train_test in train_test_arr:
@@ -177,37 +283,37 @@ for train_test in train_test_arr:
     print('Train: ', format(test_size, '.3f'), '%  Test: ', format(100-test_size, '.3f'), '%')
     print('=============================================================================\n')
 
-    DecisionTreeClassifier_model.fit(train_test[0], train_test[2])
-    KNN_model.fit(train_test[0], train_test[2])
-    MLPClassifier_model.fit(train_test[0], train_test[2])
+    # Fit models to training data.
+    model_DecisionTreeClassifier.fit(train_test[0], train_test[2])
+    model_KNeighborsClassifier.fit(train_test[0], train_test[2])
+    model_MLPClassifier.fit(train_test[0], train_test[2])
+    model_LinearRegression.fit(train_test[0], train_test[2])
+    model_SGDRegressor.fit(train_test[0], train_test[2])
 
-    # Create Predictions.
-    DecisionTreeClassifier_prediction = DecisionTreeClassifier_model.predict(train_test[1])
-    KNN_prediction = KNN_model.predict(train_test[1])
-    MLPClassifier_prediction = MLPClassifier_model.predict(train_test[1])
-
-    # Print Accuracy.
-    print("Decision Tree Accuracy:         ", accuracy_score(DecisionTreeClassifier_prediction, train_test[3]))
-    print("K-Nearest Neighbour Accuracy:   ", accuracy_score(KNN_prediction, train_test[3]))
-    print("Multilayer Perceptron Accuracy: ", accuracy_score(MLPClassifier_prediction, train_test[3]))
+    # Test models against test data.
+    print("Decision Tree Accuracy:         ", model_DecisionTreeClassifier.score(train_test[1], train_test[3]))
+    print("K-Nearest Neighbour Accuracy:   ", model_KNeighborsClassifier.score(train_test[1], train_test[3]))
+    print("Multilayer Perceptron Accuracy: ", model_MLPClassifier.score(train_test[1], train_test[3]))
+    print("Linear Regression Accuracy:     ", model_LinearRegression.score(train_test[1], train_test[3]))
+    print("SGD Regression Accuracy:        ", model_SGDRegressor.score(train_test[1], train_test[3]))
 
     # Only create the plots if set to do so.
     if not do_plots:
         continue
 
     # Plot the first model.
-    DecisionTreeClassifier_Display = RocCurveDisplay.from_estimator(DecisionTreeClassifier_model, train_test[1], train_test[3])
+    display_DecisionTreeClassifier = RocCurveDisplay.from_estimator(model_DecisionTreeClassifier, train_test[1], train_test[3])
 
     # For some reason, if we don't first make a dummy plot with just the first model, this will appear twice on the
     # later plot. Comment the below line and run to see what I mean. I can't seem to find a fix for this, and the
     # scikit docs example shows it this way as well.
     plt.show()
 
-    # Plot the remaining two models on top of the first model.
+    # Plot the remaining models on top of the first model.
     ax = plt.gca()
-    KNN_Display = RocCurveDisplay.from_estimator(KNN_model, train_test[1], train_test[3], ax=ax, alpha=0.8)
-    MLPClassifier_Display = RocCurveDisplay.from_estimator(MLPClassifier_model, train_test[1], train_test[3], ax=ax, alpha=0.8)
-    DecisionTreeClassifier_Display.plot(ax=ax, alpha=0.8)
+    display_KNeighborsClassifier = RocCurveDisplay.from_estimator(model_KNeighborsClassifier, train_test[1], train_test[3], ax=ax, alpha=0.8)
+    display_MLPClassifier = RocCurveDisplay.from_estimator(model_MLPClassifier, train_test[1], train_test[3], ax=ax, alpha=0.8)
+    display_DecisionTreeClassifier.plot(ax=ax, alpha=0.8)
 
     # Show the comparison of all models.
     plt.show()
